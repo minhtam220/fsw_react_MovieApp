c0dfcadb11c6ee8169705cbf3594c7e6a070ffe7
minhtam220 <minhtam220@gmail.com>
2023-04-15 19:13:14 +0700

    modify readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+  let x = 0;
+
+  const handleClick = () => {
+    setCount(count + 1);
+    x++;
+    console.log("count inside handleClick", count);
+    console.log("x inside handleClick", x);
+  };
+
+  console.log("count outside handleClick", count);
+  console.log("x outside handleClick", x);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>{count}</div>
+        <button onClick={handleClick}>+</button>
       </header>
     </div>
   );
 }
-
-export default App;
