c30fe66389019d470ddbcf04e0099a8de0f9f7a1
minhtam220 <minhtam220@gmail.com>
2023-04-18 20:15:43 +0700

    clone from coderstore

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useState } from "react";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes";
+import { AuthProvider, ThemeProvider } from "./contexts";
+//import ThemeProvider from "./contexts/ThemeProvider";
 
-export default function App() {
-  const [count, setCount] = useState(0);
-  let x = 0;
-
-  const handleClick = () => {
-    setCount(count + 1);
-    x++;
-    console.log("count inside handleClick", count);
-    console.log("x inside handleClick", x);
-  };
-
-  console.log("count outside handleClick", count);
-  console.log("x outside handleClick", x);
-
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>{count}</div>
-        <button onClick={handleClick}>+</button>
-      </header>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <ThemeProvider>
+          <Router />
+        </ThemeProvider>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
+
+export default App;
---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,106 @@
+import { createContext, useReducer, useEffect } from "react";
+
+//initialize the state
+const initialState = {
+  isAuthenticated: false,
+  isInitialized: false,
+  user: null,
+};
+
+const INITIALIZE = "INITIALIZE";
+const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+const LOGOUT = "LOGOUT";
+
+//function reducer
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INITIALIZE:
+      const { isAuthenticated, user } = action.payload;
+      return {
+        ...state,
+        isAuthenticated,
+        isInitialized: true,
+        user,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.user,
+      };
+    case LOGOUT:
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      };
+    default:
+      return state;
+  }
+};
+
+//declare the context and set initial values
+const AuthContext = createContext({ ...initialState });
+
+function AuthProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const initialize = async () => {
+      try {
+        const username = window.localStorage.getItem("username");
+
+        if (username) {
+          dispatch({
+            type: INITIALIZE,
+            payload: { isAuthenticated: true, user: { username } },
+          });
+        } else {
+          dispatch({
+            type: INITIALIZE,
+            payload: { isAuthenticated: false, user: null },
+          });
+        }
+      } catch (err) {
+        console.error(err);
+        dispatch({
+          type: INITIALIZE,
+          payload: {
+            isAuthenticated: false,
+            user: null,
+          },
+        });
+      }
+    };
+    initialize();
+  }, []);
+
+  const login = async (username, callback) => {
+    window.localStorage.setItem("username", username);
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: { user: { username } },
+    });
+    callback();
+  };
+
+  const logout = async (callback) => {
+    window.localStorage.removeItem("username");
+    dispatch({ type: LOGOUT });
+    callback();
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        ...state,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export { AuthContext, AuthProvider };
